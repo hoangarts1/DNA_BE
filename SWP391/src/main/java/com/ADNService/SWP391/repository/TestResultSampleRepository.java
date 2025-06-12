c40cd90e023d026c18169b2267ffb4b92c392d50
nguyenphiduy <duynpse180529@fpt.edu.vn>
2025-06-12 16:00:21 +0700

    TestResultSample

---
+++ SWP391/src/main/java/com/ADNService/SWP391/repository/TestResultSampleRepository.java
@@ -0,0 +1,7 @@
+package com.ADNService.SWP391.repository;
+
+import com.ADNService.SWP391.entity.TestResultSample;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestResultSampleRepository extends JpaRepository<TestResultSample, Long> {
+}
